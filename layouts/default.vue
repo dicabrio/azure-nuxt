68ea0b5c040fc11f92bcc35e980066d926cf3b3f
Robert Cabri <robert@dicabrio.com>
2019-02-19 10:36:35 +0100

    Initial Start

---
+++ layouts/default.vue
@@ -0,0 +1,142 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+
+:root {
+  --input-padding-x: 0.75rem;
+  --input-padding-y: 0.75rem;
+}
+
+html,
+body {
+  height: 100%;
+}
+
+body {
+  display: -ms-flexbox;
+  display: -webkit-box;
+  display: flex;
+  -ms-flex-align: center;
+  -ms-flex-pack: center;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 420px;
+  padding: 15px;
+  margin: 0 auto;
+}
+
+.form-label-group {
+  position: relative;
+  margin-bottom: 1rem;
+}
+
+.form-label-group > input,
+.form-label-group > label {
+  padding: var(--input-padding-y) var(--input-padding-x);
+}
+
+.form-label-group > label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  margin-bottom: 0; /* Override default `<label>` margin */
+  line-height: 1.5;
+  color: #495057;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+  transition: all 0.1s ease-in-out;
+}
+
+.form-label-group input::-webkit-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input:-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-moz-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::placeholder {
+  color: transparent;
+}
+
+.form-label-group input:not(:placeholder-shown) {
+  padding-top: calc(var(--input-padding-y) + var(--input-padding-y) * (2 / 3));
+  padding-bottom: calc(var(--input-padding-y) / 3);
+}
+
+.form-label-group input:not(:placeholder-shown) ~ label {
+  padding-top: calc(var(--input-padding-y) / 3);
+  padding-bottom: calc(var(--input-padding-y) / 3);
+  font-size: 12px;
+  color: #777;
+}
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,74 @@
+<template>
+  <form class="form-signin" @submit.prevent="submitForm">
+    <div class="text-center mb-4">
+      <img
+        class="mb-4"
+        src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg"
+        alt
+        width="72"
+        height="72"
+      >
+      <h1 class="h3 mb-3 font-weight-normal">Floating labels</h1>
+      <p>
+        Build form controls with floating labels via the
+        <code>:placeholder-shown</code> pseudo-element.
+        <a
+          href="https://caniuse.com/#feat=css-placeholder-shown"
+        >Works in latest Chrome, Safari, and Firefox.</a>
+      </p>
+    </div>
+
+    <div class="form-label-group">
+      <input
+        id="inputEmail"
+        type="email"
+        name="email"
+        class="form-control"
+        placeholder="Email address"
+        v-model="username"
+        v-validate="'required|email'"
+        autofocus
+      />
+      <label for="inputEmail">Email address</label>
+    </div>
+    <div class="text-danger">
+      <span>{{ errors.first('email') }}</span>
+    </div>
+
+    <div class="form-label-group">
+      <input
+        id="inputPassword"
+        type="password"
+        class="form-control"
+        placeholder="Password"
+        v-model="password"
+        required
+      />
+      <label for="inputPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" v-model="remember" /> Remember me
+      </label>
+    </div>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 text-muted text-center">&copy; 2017-2018</p>
+  </form>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      remember: false
+    }
+  },
+  methods: {
+    submitForm(e) {
+      window.console.log(this.username, this.password, this.remember)
+    }
+  }
+}
+</script>
